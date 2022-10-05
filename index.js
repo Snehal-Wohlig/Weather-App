d709718f1c6a528341a95287a752a86eb803bf8a
Snehal-Wohlig <snehal.kamble@wohlig.com>
2022-10-05 23:41:15 +0530

    add dumi weather data

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const fs =require("fs")
 const frontEnd =fs.readFileSync("index.html","utf-8")
 
 const server =http.createServer((req,res) => {
-   
+   if(req.url=="/"){
+    requests('https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=66bf36e593f6141fa9e35e032a57b977')
+.on('data', (chunk) => {
+  let objdata = JSON.parse(chunk);
+  let arrData = [objdata];
+  console.log(arrData)
 })
+.on('end', (err) => {
+  if (err) return console.log('connection closed due to errors', err);
+ 
+  console.log('end');
+});
+   }
+})
+
+server.listen(3000,"127.0.0.1")
